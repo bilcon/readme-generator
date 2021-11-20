de47275f7894cfbaf8c88f0e18c54a91f7fb6df6
Bill Morris <bilcon@swbell.net>
2021-11-20 15:01:05 -0600

    added util folder and begin questions

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Please provide a project title.  (Required)',
+    validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Please provide a project title!');
+            return false;
+        }
+    }
+}
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
