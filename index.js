ed2bf88894c1d89fce251b2e49ff2d63a00cebe7
Bill Morris <bilcon@swbell.net>
2021-11-21 14:35:19 -0600

    added functions writefile and init

--- index.js
+++ index.js
@@ -1,27 +1,114 @@
-// TODO: Include packages needed for this application
+// Global require's
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown.js');
+const generateMarkdown = require('./src/generateMarkdown.js')
 
-// TODO: Create an array of questions for user input
+// Array of questions to ask the user
 const questions = [
-  {
-    type: 'input',
-    name: 'title',
-    message: 'Please provide a project title.  (Required)',
-    validate: nameInput => {
-        if (nameInput) {
-            return true;
-        } else {
-            console.log('Please provide a project title!');
-            return false;
+    // Project name
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of the project? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('You need to enter a title to continue!');
+                return false;
+            }
         }
-    }
     },
+    // Description of project
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('You need to provide a project description!');
+                return false;
+            }
+        }
+    },
+    // Installation Instructions
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install your project? (Required)',
+        validate: installationInput => {
+            if (installationInput) {
+                return true;
+            } else {
+                console.log('You need to provide installation info to continue!');
+                return false;
+            }
+        }
+    },
+    // Usage Information
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this project? (Required)',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('You need to provide information on how to use project!');
+                return false;
+            }
+        }
+    },
+    // Contribution Guidlines
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How should people contribute to this project? (Required)',
+        validate: contributionInput => {
+            if (contributionInput) {
+                return true;
+            } else {
+                console.log('You need to provide information on how to contribute to the project!');
+                return false;
+            }
+        }
+    },
+    // Test Instructions 
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'How do you test this project? (Required)',
+        validate: testingInput => {
+            if (testingInput) {
+                return true;
+            } else {
+                console.log('You need to describe how to test this project!');
+                return false;
+            }
+        }
+    },
+    // License Options
+    {
+        type: 'checkbox',
+        name: 'licensing',
+        message: 'Choose a license for your project (Required)',
+        choices: ['Apache', 'MIT', 'Mozilla-Public', 'GNU-General-Public', 'Common-Development-and Distribution', 'None'],
+        validate: licensingInput => {
+            if (licensingInput) {
+                return true;
+            } else {
+                console.log('You must pick a license for the project!');
+                return false;
+            }
+        }
+    },
+    // Github Username
     {
         type: 'input',
         name: 'github',
-        message: 'Please enter your GitHub username. (Required)',
+        message: 'Enter your GitHub Username (Required)',
         validate: githubInput => {
             if (githubInput) {
                 return true;
@@ -31,135 +118,31 @@ const questions = [
             }
         }
     },
+    // Email Address
     {
         type: 'input',
-        name: 'repo',
-        message: 'Please enter the name of your repo. (Required)',
-        validate: repoInput => {
-            if (repoInput) {
-                return true;
-            } else {
-                console.log('Please enter the name of your repo!')
-            }
-        }
+        name: 'email',
+        message: 'Would you like to include your email?',
     },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Provide a description of your application. (Required)',
-        validate: descInput => {
-            if (descInput) {
-                return true;
-            } else {
-                console.log('Please enter a description!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Please provide information for using your application. (Required)',
-        validate: usageInput => {
-            if (usageInput) {
-                return true;
-            } else {
-                console.log('Please provide information for using your application!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'checkbox',
-        name: 'contents',
-        message: 'Any additional sections you would like to include in your README?',
-        choices: [
-            {
-                name: 'Deployed Application',
-                checked: false
-            },
-            {
-                name: 'Installation',
-                checked: false
-            },
-            {
-                name: 'Screenshots',
-                checked: true
-            },
-            {
-                name: 'Built With',
-                checked: true
-            },
-            {
-                name: 'License',
-                checked: false
-            },
-            {
-                name: 'Contributing',
-                checked: false
-            },
-            {
-                name: 'Tests',
-                checked: false
-            },
-            {
-                name: 'Questions',
-                checked: true
-            },
-            {
-                name: 'Credits',
-                checked: true
-            },
-        ]
-    },
-    {
-        type: 'input',
-        name: 'link',
-        message: 'Please provide a link to your deployed application.',
-        when: ({ contents }) => {
-            if (contents.indexOf('Deployed Application') > -1) {
-                return true;
-            } else { 
-                return false;
-            }
-        },
-        validate: linkInput => {
-            if (linkInput) {
-                return true;
-            } else {
-                console.log('Please enter a link!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'Please list any required packages for installation of your application.',
-        when: ({ contents }) => {
-            if (contents.indexOf('Installation') > -1) {
-                return true;
-            } else {
-                return false;
-            }
-        },
-        validate: installInput => {
-            if (installInput) {
-                return true;
-            } else {
-                console.log('Please enter installation instructions!');
-                return false;
-            }
-        }
-    },
-
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err)
+            throw err;
+        console.log('Success! Information transferred to the README!')
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function (userInput) {
+        console.log(userInput)
+        writeToFile("README.md", generateMarkdown(userInput));
+    });
+};
 
 // Function call to initialize app
 init();
